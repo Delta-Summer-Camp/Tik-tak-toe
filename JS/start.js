72484c9f3dae1d4d9123a822af8835d10ade9b16
Dima <d.ablov@gmail.com>
2021-11-22 12:35:37 +0300

    Итоги занятия 22 ноября. Умеем ставить "крестик"!

--- JS/start.js
+++ JS/start.js
@@ -1,6 +1,11 @@
-function init() {
-	let ch = document.getElementById("changeable");
-	ch.onclick = function() {
-		ch.innerHTML = "Меня кликнули!";
+window.onload = function() {
+
+	newGame();
+}
+
+function newGame() {
+	const cell_11 = document.getElementById("cell-11");
+	cell_11.onclick = function() {
+		cell_11.style.backgroundImage = "url('./IMG/x.gif')";
 	}
 }
