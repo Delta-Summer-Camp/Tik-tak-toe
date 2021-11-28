62ad793d13f9364d30ddf0889ac57f25ced18205
Dima <d.ablov@gmail.com>
2021-11-28 18:39:51 +0300

    Промежуточные результаты занятия 28 ноября

--- JS/start.js
+++ JS/start.js
@@ -1,11 +1,19 @@
+let cellStatus = 0;
+
 window.onload = function() {
 
 	newGame();
 }
 
 function newGame() {
-	const cell_11 = document.getElementById("cell-11");
-	cell_11.onclick = function() {
-		cell_11.style.backgroundImage = "url('./IMG/x.gif')";
+	const cell_00 = document.getElementById("cell-00");
+	cell_00.onclick = function() {
+		if (cellStatus === 0 || cellStatus === -1) {
+			cell_00.style.backgroundImage = "url('./IMG/x.gif')";
+			cellStatus = 1;
+		} else {
+			cell_00.style.backgroundImage = "url('./IMG/o.gif')";
+			cellStatus = -1;			
+		}
 	}
 }
