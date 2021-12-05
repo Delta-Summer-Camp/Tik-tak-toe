9dea8da6f4313317c4956961b6a9a04e3df81b39
Dima <d.ablov@gmail.com>
2021-12-05 20:05:10 +0300

    Программа с занятия 5 декабря: последовательно ставятся крестики и нолики, можно сбросить кнопкой.

--- JS/start.js
+++ JS/start.js
@@ -1,5 +1,6 @@
 const gameSize = 3;
 const cells = [[0,0,0],[0,0,0],[0,0,0]];
+let currentPlayer = +1;
 
 window.onload = function() {
 
@@ -11,14 +12,29 @@ function newGame() {
 		for(let j = 0; j < gameSize; j++) {
 			const cell = document.getElementById("cell-" + i + j);
 			cell.onclick = function() {
-				if (cells[i][j] === 0 || cells[i][j] === -1) {
-					cell.style.backgroundImage = "url('./IMG/x.gif')";
-					cells[i][j] = 1;
-				} else {
-					cell.style.backgroundImage = "url('./IMG/o.gif')";
-					cells[i][j] = -1;			
+				if (cells[i][j] === 0) {
+					if (currentPlayer === +1) {
+						cell.style.backgroundImage = "url('./IMG/x.gif')";
+						cells[i][j] = 1;
+						currentPlayer = -1;
+					} else {
+						cell.style.backgroundImage = "url('./IMG/o.gif')";
+						cells[i][j] = -1;
+						currentPlayer = +1;						
+					}
 				}
 			}
 		}
+	}
+	const button = document.getElementById("newGame");
+	button.onclick = function() {
+		for(let i = 0; i < gameSize; i++) {
+			for(let j = 0; j < gameSize; j++) {
+				const cell = document.getElementById("cell-" + i + j);
+				cell.style.backgroundImage = "none";
+				cells[i][j] = 0;
+			}
+		}
+		currentPlayer = +1;
 	}
 }
